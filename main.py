a056f7876cf00f85226e37e9688a4afb13d40642
Jinduk <35905280+jindeok@users.noreply.github.com>
2022-08-08 13:47:43 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -39,11 +39,11 @@ def parse_args():
                          help="Make sure the alignment numbering start from 0")
     parser.add_argument('--k_hop', nargs='?', default= 2)  
     parser.add_argument('--hid_dim', nargs='?', default=150) 
-    parser.add_argument('--train_ratio', nargs='?', default= 0) 
+    parser.add_argument('--train_ratio', nargs='?', default= 0.05) 
     parser.add_argument('--graphname', nargs='?', default='fb-tt') 
     parser.add_argument('--mode', nargs='?', default='not_perturbed', help="not_perturbed or perturbed") 
-    parser.add_argument('--edge_portion', nargs='?', default=0.1,  help="a param for the perturbation case")  
-    parser.add_argument('--att_portion', nargs='?', default=0.2,  help="a param for the perturbation case")
+    parser.add_argument('--edge_portion', nargs='?', default=0.2,  help="a param for the perturbation case")  
+    parser.add_argument('--att_portion', nargs='?', default=0.1,  help="a param for the perturbation case")
     
     return parser.parse_args()
 
@@ -55,24 +55,24 @@ args = parse_args()
 
 if __name__ == "__main__":
         
-    G1, G2, attr1, attr2, alignment_dict, alignment_dict_reversed, idx1_dict, idx2_dict = na_dataloader(args)
-    #attr1_aug, attr2_aug = augment_attr(G1, G2, attr1, attr2, interval = 3)
-    attr1_aug_1, attr2_aug_1 = augment_attr_khop(G1, G2, attr1, attr2, interval = 2, k = 1)
-    attr1_aug_2, attr2_aug_2 = augment_attr_khop(G1, G2, attr1, attr2, interval = 3, k = 2)
-    attr1_aug = np.concatenate((attr1_aug_1, attr1_aug_2), axis=1)
-    attr2_aug = np.concatenate((attr2_aug_1, attr2_aug_2), axis=1)
-    #attr1_aug, attr2_aug = augment_attr_Katz(G1, G2, attr1, attr2, interval = 2)
+    G1, G2, attr1, attr2, alignment_dict, alignment_dict_reversed, idx1_dict, idx2_dict \
+        = na_dataloader(args)
+    attr1_aug, attr2_aug = augment_attributes(G1, G2,
+                                              attr1, attr2,
+                                              num_attr = 15,
+                                              version = "Katz",     # "Katz" or "khop"
+                                              khop = 1,
+                                              normalize = False) 
+    attr1_aug, attr2_aug = aug_trimming(attr1_aug, attr2_aug)
+
+    #Checking statistics
+#    str_con_portion = struct_consist_checker(G1, G2, alignment_dict)
+#    att_con_portion = att_consist_checker(G1, G2, attr1, attr2, idx1_dict, idx2_dict, alignment_dict)
+#    feat_avg_diff = feat_diff_checker(attr1_aug, attr2_aug)
     
     GradAlign = GradAlign(G1, G2, attr1, attr2, attr1_aug, attr2_aug, args.k_hop, args.hid_dim, alignment_dict, alignment_dict_reversed, \
-                                      args.train_ratio, idx1_dict, idx2_dict, alpha = G2.number_of_nodes() / G1.number_of_nodes(), beta = 1) 
-    S_mv, S_prime, seed_list1, seed_list2, results = GradAlign.run_algorithm()
-
-
-
-
-
-
-
+                                      args.train_ratio, idx1_dict, idx2_dict, alpha = G2.number_of_nodes() / G1.number_of_nodes(), beta = 1)    
+    GradAlign.run_algorithm()
 
 
 
